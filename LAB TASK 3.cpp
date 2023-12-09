48b51012ad34fc8d49a8700c3b699967605aecd6
Ikrash-Bakhsh <153372614+Ikrash-Bakhsh@users.noreply.github.com>
2023-12-09 19:37:55 +0500

    Add files via upload

---
+++ LAB TASK 3.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+	system("color 13");
+    cout<<" $$$$$$$$$$$$         $$$$$$$$$$$$$$          "<<endl;
+	cout<<"     $$               $$           $$         "<<endl;
+	cout<<"     $$               $$            $$        "<<endl;
+	cout<<"     $$               $$             $        "<<endl;
+  	cout<<"     $$               $$            $$        "<<endl;
+	cout<<"     $$               $$           $$         "<<endl;
+	cout<<"     $$               $$$$$$$$$$$$$$          "<<endl;
+	cout<<"     $$               $$           $$         "<<endl;
+	cout<<"     $$               $$            $$        "<<endl;
+	cout<<"     $$               $$             $        "<<endl;
+	cout<<"     $$               $$            $$        "<<endl;
+	cout<<"     $$               $$            $$        "<<endl;		
+	cout<<" $$$$$$$$$$$$         $$$$$$$$$$$$$$$         "<<endl;          
+  return 0;
+}
